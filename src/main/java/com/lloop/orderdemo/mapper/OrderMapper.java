827bb7aa9c2d9b8befe15af6801e9a792fbf95fd
lloop <alecleeeeee@gmail.com>
2025-04-18 01:43:03 +0800

    解决 MyBatisPlus 依赖不兼容等问题

--- src/main/java/com/lloop/orderdemo/mapper/OrderMapper.java
+++ src/main/java/com/lloop/orderdemo/mapper/OrderMapper.java
@@ -2,11 +2,13 @@ package com.lloop.orderdemo.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.lloop.orderdemo.model.entity.UserOrder;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @Author lloop
  * @Create 2025/4/18 00:05
  */
+@Mapper
 public interface OrderMapper extends BaseMapper<UserOrder> {
 
 }
