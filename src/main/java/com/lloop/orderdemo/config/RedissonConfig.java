ccf176266f3faf9c6a5c60eead04a6ff262304eb
lloop <alecleeeeee@gmail.com>
2025-04-18 01:44:14 +0800

    解决Redisson连接 Redis 集群问题,目前仍然没有解决 Redisson访问Docker内网IP问题

--- src/main/java/com/lloop/orderdemo/config/RedissonConfig.java
+++ src/main/java/com/lloop/orderdemo/config/RedissonConfig.java
@@ -16,12 +16,12 @@ public class RedissonConfig {
         // 集群模式配置，适用于分布式环境
         config.useClusterServers()
             .addNodeAddress(
-                    "redis://127.0.0.1:7101",  // 声明主节点在宿主机的ip和端口号
+                    "redis://127.0.0.1:7101",
                     "redis://127.0.0.1:7102",
                     "redis://127.0.0.1:7103"
             )
             .setMasterConnectionPoolSize(64)  // 设置连接池
-            .setScanInterval(2000);   // 设置节点状态扫描间隔
+            .setScanInterval(10000);   // 设置节点状态扫描间隔
             // .setPassword("my-redis-password"); // 如果有密码
 
         return Redisson.create(config);
